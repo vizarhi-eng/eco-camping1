4a3c7a06013cddbb662f969cd71c8b4a531cdc32
vizarhi-eng <vi.zarhi@duocuc.cl>
2026-05-26 21:59:03 -0400

    Add files via upload

--- ecocamping.py
+++ ecocamping.py
@@ -7,6 +7,7 @@ while ejecutando:
     print("1.- Ver sitios disponibles")
     print("2.- Registro de nuevos vehiculos en el sitio(Entrada)")
     print("3.- Registro de salida de vehiculos(Salida)")
+    print("4.- estado actual del cmaping")
     print("5.- Salir")
     try:
         opcion = int(input("Seleccione una opcion (1-5):"))
@@ -33,5 +34,28 @@ while ejecutando:
                     print(f"Ingreso registrado, se han ocupado {ingreso} de sitios")
             except ValueError:
                 print(f"Error: debe ingresar un numero valido")
+    elif opcion == 3:
+        print(f"\n-- Registrar salidad (Vehiculos o sitios ocupados: {sitios_ocupados})")
+        if sitios_ocupados == 0:
+            print("no hay vehiculos registrados en el camping actualmente")
+        else:
+            try:
+                salida = int(input("¿cuantos vehiculos se retiran?:"))
+                if salida <= 0:
+                    print(f"Error: la cantidad a retirar debe ser mayor a 0")
+                elif salida > sitios_ocupados:
+                    print(f"Error: no se puede retirar mas de {sitios_ocupados} vehiculos")
+                else:
+                    sitios_ocupados -=salida
+                    print(f"Salida registrada, se han liberado {salidad} sitios")
+            except ValueError:
+                print("Error: Debe ingresar un numero entero valido")
+    elif opcion == 4:
+        porcentaje_ocupacion = (sitios_ocupados / capacidad_maxima) * 100
+        print(f"\n[ESTADO] Ocupacion actual: {sitios_ocupados}/{capacidad_maxima} sitios")
+        print(f"[ESTADO] El camping esta al {porcentaje_ocupacion:.if}% de su capacidad")
+    elif opcion == 5:
+        print("Cerrando el sistema")
+        ejecutando = False
     else:
         print("Opcion fuera de rango")
